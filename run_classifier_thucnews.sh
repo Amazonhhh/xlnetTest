5ea6c2e8e517173a4c25936b1f409aa2d1ea8161
Amazonhhh <38023469+Amazonhhh@users.noreply.github.com>
2019-10-22 19:43:52 +0800

    Add files via upload
    xlnet modified file.

---
+++ run_classifier_thucnews.sh
@@ -0,0 +1,27 @@
+export XLNET_DIR=/home/ccnunlp-e/lil/GLUE/chineseGLUE/baselines/models/xlnet/prev_trained_model/chinese_xlnet_mid_L-24_H-768_A-12
+export RAW_DIR=/home/ccnunlp-e/lil/GLUE/chineseGLUE/baselines/models/xlnet/chineseGLUEdatasets
+TASK_NAME="thucnews"
+OUTPUT_DIR=/home/ccnunlp-e/lil/GLUE/chineseGLUE/baselines/models/xlnet/thucnews_output
+python run_classifier.py \
+    --spiece_model_file=${XLNET_DIR}/spiece.model \
+    --model_config_path=${XLNET_DIR}/xlnet_config.json \
+    --init_checkpoint=${XLNET_DIR}/xlnet_model.ckpt \
+    --task_name=$TASK_NAME \
+    --do_train=True \
+    --do_eval=True \
+    --do_predict=True \
+    --eval_all_ckpt=False \
+    --uncased=False \
+    --data_dir=${RAW_DIR}/${TASK_NAME} \
+    --output_dir=${OUTPUT_DIR} \
+    --model_dir=${OUTPUT_DIR} \
+    --train_batch_size=16 \
+    --eval_batch_size=16 \
+    --num_hosts=1 \
+    --num_core_per_host=1 \
+    --num_train_epochs=3 \
+    --max_seq_length=128 \
+    --learning_rate=2e-5 \
+    --save_steps=1000 \
+    --use_tpu=False \
+    --predic_dir=${OUTPUT_DIR}/predict
